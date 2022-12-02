2d423d4c10edb472f63004eaa098e09b08cf8bc4
Vu Dinh Thai Hung <skek.git@gmail.com>
2022-12-02 22:28:47 +0700

    start

---
+++ content/hero/index.md
@@ -0,0 +1,8 @@
+---
+title: 'Hello, my name is'
+name: 'Vu Dinh Thai Hung'
+subtitle: 'I love exploring new things!'
+buttonText: 'Get In Touch'
+---
+
+I'm a freshman at Vinschool Central Park. Primarily interested in Web Development and Computer Science.
---
+++ src/config.js
@@ -0,0 +1,81 @@
+module.exports = {
+  siteTitle: 'YVu Dinh Thai Hung',
+  siteDescription:
+    'Yashita Namdeo is an incoming Software Developer, based in India, who loves learning new things and helping tech beginners.',
+  siteKeywords:
+    'Yashita Namdeo, Yashita, Namdeo, yashitanamdeo, software engineer, web developer, javascript, python, java, svvv, indore',
+  siteUrl: 'https://sk-ek.github.io/',
+  siteLanguage: 'en_US',
+  googleAnalyticsID: 'UA-45666519-2',
+  googleVerification: 'DCl7VAf9tcz6eD9gb67NfkNnJ1PKRNcg8qQiwpbx9Lk',
+  name: 'Vu Dinh Thai Hung',
+  location: 'Vietnam',
+  email: 'nothingh27@gmail.com',
+  github: 'https://github.com/sk-ek',
+  twitterHandle: '@',
+  socialMedia: [
+    {
+      name: 'GitHub',
+      url: 'https://github.com/sk-ek',
+    },
+    {
+      name: 'Linkedin',
+      url: 'https://www.youtube.com/watch?v=eBGIQ7ZuuiU',
+    },
+    {
+      name: 'Codepen',
+      url: 'https://www.youtube.com/watch?v=eBGIQ7ZuuiU',
+    },
+    {
+      name: 'Instagram',
+      url: 'https://www.youtube.com/watch?v=eBGIQ7ZuuiU',
+    },
+    {
+      name: 'Twitter',
+      url: 'https://www.youtube.com/watch?v=eBGIQ7ZuuiU',
+    },
+  ],
+
+  navLinks: [
+    {
+      name: 'About',
+      url: '/#about',
+    },
+    {
+      name: 'Experience',
+      url: '/#jobs',
+    },
+    {
+      name: 'Projects',
+      url: '/#projects',
+    },
+    {
+      name: 'Contact',
+      url: '/#contact',
+    },
+  ],
+
+  navHeight: 100,
+
+  colors: {
+    green: '#64ffda',
+    navy: '#0a192f',
+    darkNavy: '#020c1b',
+  },
+
+  srConfig: (delay = 200) => ({
+    origin: 'bottom',
+    distance: '20px',
+    duration: 500,
+    delay,
+    rotate: { x: 0, y: 0, z: 0 },
+    opacity: 0,
+    scale: 1,
+    easing: 'cubic-bezier(0.645, 0.045, 0.355, 1)',
+    mobile: true,
+    reset: false,
+    useDelay: 'always',
+    viewFactor: 0.25,
+    viewOffset: { top: 0, right: 0, bottom: 0, left: 0 },
+  }),
+};
